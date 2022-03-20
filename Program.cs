ef8365686d9c8a299962d626a674071f9060c646
Eder <chlrobert93@gmail.com>
2022-03-20 14:33:39 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+
+
+namespace Rendimiento
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList objectos = new ArrayList();
+
+            objectos.Add(50); //boxing
+
+            int x = (int)objectos[0];//unboxing
+
+            foreach(int i in objectos)
+            {
+
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
